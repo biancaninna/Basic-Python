9af91e61dd0207cf8d1fc4157c9d03e0dd403ff2
Biancaninna <munazillanina123@gmail.com>
2025-01-16 18:07:10 +0700

    first commit

---
+++ Basic Pyhton/Biodata.py
@@ -0,0 +1,15 @@
+# Basic python example to create a bio
+
+Name = input ("My Name is ")
+Nickname = input ("You can call me ")
+Age  = int(input("I'am "))
+Weight = float(input ("I'am (in kg) "))
+Address = input ("I'am from ")
+
+# How to display biodata using python programming language
+
+print (f"My Name is {Name}")
+print (f"You can call me {Nickname}")
+print (f"I'am {Age} years old")
+print (f"I'am {Weight} kg")
+print (f"I'am from {Address}")
